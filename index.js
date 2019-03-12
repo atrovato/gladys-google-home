6d7936a6e957dd4772d79b5866774c2c15750c3d
Alexandre Trovato <alexandre.trovato@gmail.com>
2019-03-12 22:35:15 +0100

    Setup as notification system

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const say = require('./lib/say.js');
 const notify = require('./lib/notify.js');
 const setup = require('./lib/setup.js');
+const init = require('./lib/init.js');
 
 module.exports = function () {
   gladys.on('ready', function () {
-    setup();
+    init();
   });
 
   return {
